083469355f66d7e903098fa6318afbd1d6446ce5
kjeeze <kddavis@mtu.edu>
2017-01-18 15:28:59 -0500

    Name added to participant.c

--- code/participants.c
+++ code/participants.c
@@ -94,6 +94,8 @@ main (int argc, char *argv[])
   printf ("%02d: Karan Desai\n", ++i);
 
   printf ("%02d: Riley Mulford\n", ++i);
+  
+  printf ("%02d:Kevin Usher\n", ++i);
 
   printf ("There are %i participants.\n", i);
 
