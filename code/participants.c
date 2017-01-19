accd84ebdd9afd4081e016168d0ead416a6ff2cc
Connor Przybyla <cgprzyby@mtu.edu>
2017-01-18 22:47:28 -0500

    Updated participants.c

--- code/participants.c
+++ code/participants.c
@@ -20,6 +20,7 @@ main (int argc, char *argv[])
   int i = 0;
 
   printf ("%02d: Alex David\n", ++i);
+
   printf ("%02d: Jacob Gould\n",++i);
 
   printf ("%02d: Brendan Hansknecht\n",++i);
@@ -121,6 +122,8 @@ main (int argc, char *argv[])
   printf ("%02d: Tyler Schlomer\n", ++i);
 
   printf("%02d: Alec Hitchiner\n", ++i);
+
+  printf("%02d: Connor Przybyla\n", ++i);
   
   printf ("There are %i participants.\n", i);
 
